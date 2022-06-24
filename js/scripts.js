87e551c4be5e2d05f116edf49426127ab33118dc
wiessall <tristan.wiessalla@embl.it>
2022-06-24 10:26:45 +0200

    js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    Pace.on("done:, function(){
+      $(\".pre_loader\").css(\"display\", none)
+    }")
+
+    $("#js-rotating").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "bounceIn",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 3000,
+        complete: function () {
+            // Called after the entrance animation is executed.
+        }
+    });
+})
